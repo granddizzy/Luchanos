a2a3e033c0da35c47c660f3ad018076b76fbe0c9
roman lepihov <lepihovrom@yandex.by>
2023-06-26 10:04:08 +0300

    [update] Block 3 task 2

--- Decorators/Block_3/task2.py
+++ Decorators/Block_3/task2.py
@@ -1,21 +1,29 @@
+def wrapper(*agrs1, **kwargs1):
+    def dec(func):
+        def another_func(*args, **kwargs):
+            if "text" in kwargs1:
+                print(kwargs1["text"])
+            count = 0
+
+            if "number_of_attempts" in kwargs1:
+                number_of_attempts = kwargs1["number_of_attempts"]
+            else:
+                number_of_attempts = 10
+
+            while count < number_of_attempts:
+                try:
+                    return func(*args, **kwargs)
+                except:
+                    count += 1
+
+            raise Exception(f'Пробовал {count} раз. Ошибка')
+
+        return another_func
+    return dec
+
+
+@wrapper(number_of_attempts=15, text="Покупайте наших котиков")
 def func(a, b):
     return a // b
 
-
-def dec(func, text: str, number_of_attempts: int = 10):
-    def another_func(*args, **kwargs):
-        print(text)
-        count = 0
-        while count < number_of_attempts:
-            try:
-                return func(*args, **kwargs)
-            except:
-                count += 1
-
-        raise Exception(f'Пробовал {count} раз. Ошибка')
-
-    return another_func
-
-
-test = dec(func, "Покупайте наших котиков!")
-print(test(1, 0))
+print(func(4, 0))
